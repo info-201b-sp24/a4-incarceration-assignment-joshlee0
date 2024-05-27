65d5f1ffebbf6fc0f52e8725c03afc6f7078d0e3
joshlee0 <166346380+joshlee0@users.noreply.github.com>
2024-05-26 22:17:14 -0700

    Update chart1_example.R

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,9 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+kingCounty %>%
+    ggplot(aes(x = year,)) +
+    geom_line(aes(y = black_prison_pop_rate, color = "Black")) +
+    geom_line(aes(y = aapi_prison_pop_rate, color = "AAPI")) +
+    geom_line(aes(y = white_prison_pop_rate, color = "White")) +
+    labs(title = "Prison Rates by race, King County",
+         x = "Year",
+         y = "Rate of Incarceration",
+         color = "Race")
