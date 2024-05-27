b91dfb6725ee7c415c35e9ec606045d0d2ce1d1c
joshlee0 <166346380+joshlee0@users.noreply.github.com>
2024-05-26 22:53:35 -0700

    Update chart2_example.R

--- chart2_example.R
+++ chart2_example.R
@@ -2,5 +2,8 @@ library(ggplot2)
 snohomishCounty <- waState %>%
   filter(str_detect(county_name, "Snohomish County"))
 snohomishCounty %>% 
-  ggplot(aes(x = white_jail_pop_rate, y = total_jail_pop_rate)) +
-  geom_point()
+    ggplot(aes(x = white_jail_pop_rate, y = total_jail_pop_rate)) +
+    geom_point() +
+    labs(title = "White Jail Rate vs Total Jail Rate",
+        x = "White Jail Rate",
+        y = "Total Jail Rate")
